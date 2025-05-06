b7e5747b22ea228e5f6190ff388b983ba59592d4
Gonzalo Arraigada <gonzalitomdp@hotmail.com>
2025-05-06 13:03:35 -0300

    asdadas cafe

---
+++ Assets/Scripts/UnlockedCoffeeShop.cs
@@ -0,0 +1,15 @@
+
+using UnityEngine;
+
+public class UnlockedCoffeeShop : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        ProgressManager.Instance.CoffeeShopUnlocked = true;
+
+    }
+
+}
