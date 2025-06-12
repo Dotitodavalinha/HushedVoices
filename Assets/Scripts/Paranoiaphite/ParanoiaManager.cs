930e3271bb14e3e7a3c3a56c59decfbf27332a0e
Matcha-Mousse <sayuri.ganiko@davinci.edu.ar>
2025-06-11 21:35:16 -0300

    womp womp
    cambios extra paranoia

--- Assets/Scripts/Paranoiaphite/ParanoiaManager.cs
+++ Assets/Scripts/Paranoiaphite/ParanoiaManager.cs
@@ -4,6 +4,8 @@ public class ParanoiaManager : MonoBehaviour
 {
     [SerializeField] private float paranoiaLevel = 0f;
     private ParanoiaObject[] paranoiaObjects;
+    public Material vignette;
+    public Material cameraLines;
 
     private void Awake()
     {
@@ -14,6 +16,7 @@ public class ParanoiaManager : MonoBehaviour
     {
         Debug.LogWarning("Paranoia actualizada a " + value);
         paranoiaLevel = Mathf.Clamp01(value);
+
         foreach (var obj in paranoiaObjects)
         {
             obj.SetParanoia(paranoiaLevel);
@@ -25,10 +28,14 @@ public class ParanoiaManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.L))
         {
             SetParanoia(0f);
+            vignette.SetFloat("_vig_amount", 0f);
+            cameraLines.SetFloat("_scanningLinesAmount", 0f);
         }
         if (Input.GetKeyDown(KeyCode.K))
         {
             SetParanoia(1f);
+            vignette.SetFloat("_vig_amount", 0.33f);
+            cameraLines.SetFloat("_scanningLinesAmount", 0.1f);
         }
     }
 }
