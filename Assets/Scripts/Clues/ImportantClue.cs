488bc614b16d4dd46689d88ee0518d0da432340e
Gonzalo Arraigada <gonzalitomdp@hotmail.com>
2025-06-28 02:31:08 -0300

    persistencia de data entre escenas
    ohh corcho te amo

--- Assets/Scripts/Clues/ImportantClue.cs
+++ Assets/Scripts/Clues/ImportantClue.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class ImportantClue : MonoBehaviour
 {
@@ -9,8 +11,19 @@ public class ImportantClue : MonoBehaviour
 
     void Awake()
     {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         Instance = this;
+        DontDestroyOnLoad(gameObject);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+
+        AssignCanvas();
     }
+
 
     public void ShowClueAlert()
     {
@@ -18,5 +31,24 @@ public class ImportantClue : MonoBehaviour
         GameObject alert = Instantiate(clueAlertPrefab, canvasTransform);
         alert.transform.SetAsLastSibling();
     }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        AssignCanvas();
+    }
+
+    private void AssignCanvas()
+    {
+        var canvasObj = GameObject.Find("Canvas");
+        if (canvasObj != null)
+        {
+            canvasTransform = canvasObj.transform;
+        }
+        else
+        {
+            Debug.LogWarning("No se encontró un objeto llamado 'Canvas' en la escena.");
+        }
+    }
+
 }
 
--- Assets/Scripts/Clues/PlayerClueTracker.cs
+++ Assets/Scripts/Clues/PlayerClueTracker.cs
@@ -1,20 +1,34 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class PlayerClueTracker : MonoBehaviour
 {
     public static PlayerClueTracker Instance;
-    [SerializeField] public HashSet<string> clues = new HashSet<string>();
+
+    [SerializeField] private List<string> cluesList = new List<string>();
+    private HashSet<string> clues = new HashSet<string>();
 
     private void Awake()
     {
-        if (Instance == null) Instance = this;
-        else Destroy(gameObject);
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        // Cargar la lista serializada al HashSet
+        clues = new HashSet<string>(cluesList);
     }
 
     public void AddClue(string clueID)
     {
-        clues.Add(clueID);
+        if (clues.Add(clueID))
+        {
+            cluesList.Add(clueID); // reflejar en la lista para ver en editor
+        }
     }
 
     public bool HasClue(string clueID)
