96de2a8ad74722e8a2e9f018f9fb0de26ed87719
Matcha-Mousse <sayuri.ganiko@davinci.edu.ar>
2025-08-23 16:25:09 -0300

    losiento
    luces
    camaras
    accion (menu)
    corcho

--- Assets/Scripts/camera/CameraTriggerZoneOld.cs
+++ Assets/Scripts/camera/CameraTriggerZoneOld.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class CameraTriggerZoneOld : MonoBehaviour
 {
     public RoomCameraController targetCamera;
-
+    public GameObject newLights;
+    public GameObject oldLights;
 
     private void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Player")) return;
+        newLights.SetActive(true);
+        oldLights.SetActive(false);
 
-        
         CameraManager manager = FindObjectOfType<CameraManager>();
         if (manager != null && manager.IsInitialized())
             manager.SwitchToCamera(targetCamera);
