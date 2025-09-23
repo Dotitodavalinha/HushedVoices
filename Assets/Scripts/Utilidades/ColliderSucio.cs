4655f19d5f2dcf7357713c1a6fba60750fbe45ae
FPinnau <167482300+FPinnau@users.noreply.github.com>
2025-09-22 21:18:00 -0300

    inicio dialogo ese

---
+++ Assets/Scripts/Utilidades/ColliderSucio.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static CleanItem;
+
+public class ColliderSucio : MonoBehaviour
+{
+    [SerializeField] public GameObject Dialogo;
+    private GameObject Player;
+    PlayerMovementLocker playerMovementLocker;
+
+
+
+    private void Start()
+    {
+        Player = GameObject.FindWithTag("Player");
+        playerMovementLocker = Player.GetComponent<PlayerMovementLocker>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // solo el player lo activa
+        {
+            //if (!GameManager.Instance.TryLockUI())
+             //   return;
+            //if (playerMovementLocker != null) playerMovementLocker.LockMovement();
+            Dialogo.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Dialogo.SetActive(false);
+
+                //GameManager.Instance.UnlockUI();
+                Destroy(this);
+
+            }
+        }
+
+
+
+    }
+
+}
