113d8eaa3a9a3afbcc43b23d64bb5d7262a4774f
FPinnau <167482300+FPinnau@users.noreply.github.com>
2025-11-20 02:46:06 -0300

    policias a la noche 👹

--- Assets/Scripts/PoliceBehaviour/LinternaXDD.cs
+++ Assets/Scripts/PoliceBehaviour/LinternaXDD.cs
@@ -7,6 +7,12 @@ public class LinternaXDD : MonoBehaviour
     [SerializeField] private NightManager nightManager;
     [SerializeField] public GameObject Linterna;
 
+    [SerializeField] private Animator animator;
+
+    [Header("Controllers")]
+    [SerializeField] private RuntimeAnimatorController dayController;
+    [SerializeField] private RuntimeAnimatorController nightController;
+
     void Start()
     {
         nightManager = FindObjectOfType<NightManager>();
@@ -16,5 +22,16 @@ public class LinternaXDD : MonoBehaviour
     void Update()
     {
         Linterna.SetActive(nightManager.IsNight);
+
+        if (nightManager.IsNight)
+        {
+            if (animator.runtimeAnimatorController != nightController)
+                animator.runtimeAnimatorController = nightController;
+        }
+        else
+        {
+            if (animator.runtimeAnimatorController != dayController)
+                animator.runtimeAnimatorController = dayController;
+        }
     }
 }
